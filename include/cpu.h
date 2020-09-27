9bd4b9a9adbd4a807c76636338c34efd5f102a84
Juan Carlos Sanchez Ruiz de Chavec <RXWP5657@gmail.com>
2020-09-27 00:02:31 -0500

    Implementing the remaining OpCodes, the ROM loader and the function table

--- include/cpu.h
+++ include/cpu.h
@@ -4,13 +4,48 @@
 #include <tuple>
 #include <array>
 #include <time.h> 
+#include <string>
+#include <fstream>
 #include <cstdint>
 #include <stdlib.h>
+#include <functional>
+#include <unordered_map>
 
 #include "./opcode.h"
 
 namespace chip
-{   
+{
+    /**
+     *  Chip-8 programs start at address 0x200;
+     */ 
+    const uint16_t ROM_START = 0x200;
+
+    /**
+     *  Chip-8 has a font set which can be used to 
+     *  write letters A - F and digits 0 - 9. Also,
+     *  each character is 4 pixels wide and 5 pixels
+     *  high.
+     */
+    const std::array<uint8_t, 80> font_set 
+    {{
+        0xF0, 0x90, 0x90, 0x90, 0xF0, // 0x0
+        0x20, 0x60, 0x20, 0x20, 0x70, // 0x1
+        0xF0, 0x10, 0xF0, 0x80, 0xF0, // 0x2
+        0xF0, 0x10, 0xF0, 0x10, 0xF0, // 0x3
+        0x90, 0x90, 0xF0, 0x10, 0x10, // 0x4
+        0xF0, 0x80, 0xF0, 0x10, 0xF0, // 0x5
+        0xF0, 0x80, 0xF0, 0x90, 0xF0, // 0x6
+        0xF0, 0x10, 0x20, 0x40, 0x40, // 0x7
+        0xF0, 0x90, 0xF0, 0x90, 0xF0, // 0x8
+        0xF0, 0x90, 0xF0, 0x10, 0xF0, // 0x9
+        0xF0, 0x90, 0xF0, 0x90, 0x90, // 0xA
+        0xE0, 0x90, 0xE0, 0x90, 0xE0, // 0xB
+        0xF0, 0x80, 0x80, 0x80, 0xF0, // 0xC
+        0xE0, 0x90, 0x90, 0x90, 0xE0, // 0xD
+        0xF0, 0x80, 0xF0, 0x80, 0xF0, // 0xE
+        0xF0, 0x80, 0xF0, 0x80, 0x80  // 0xF
+    }};
+
     /**
      *  Representation of the Chip-8 CPU and memory. Chip-8 has
      *  16 general purpose registers but, the VF register can't 
@@ -33,23 +68,43 @@ namespace chip
      */ 
     struct CPU
     {
-        uint8_t  DT;    // Delay Time register.
-        uint8_t  ST;    // Sound Time register.    
-        uint8_t  SP;    // Stack Pointer.
-        uint16_t PC;    // Program Counter.
-        uint16_t I;     // Special register I.
+        CPU() : DT{0}, ST{0}, SP{0}, PC{0x200},I{0}, V{}, key_pad{}, memory{}, screen{}, stack{} {}
+        uint8_t  DT; // Delay Time register.
+        uint8_t  ST; // Sound Time register.    
+        uint8_t  SP; // Stack Pointer.
+        uint16_t PC; // Program Counter.
+        uint16_t I;  // Special register I.
         std::array<uint8_t, 16> V; // General purpose registers (Vx).
+        std::array<uint8_t, 16> key_pad;
         std::array<uint8_t, 4096> memory; // memory of the program.
         std::array<uint8_t, 64 * 32> screen; // Chip-8 expects a screen of 64 by 32 pixels.
         std::array<uint16_t,16> stack; // stack for function call.
     };
 
     /**
+     * Load the character font set into memory.
+     * 
+     * @param cpu load the font set into the memory.
+     */ 
+    static inline void load_font_set(CPU& cpu)
+    {
+        for(int i = 0 ; i < font_set.size() ; i++) cpu.memory[i] = font_set[i];
+    }
+
+    /**
+     *  Do nothing.
+     */  
+    static inline void op_code_0x0(CPU& cpu, const OpCode& op_code)
+    {
+
+    }
+
+    /**
      *  Clear the pixels of the screen with the color black.
      *  
      *  @param cpu the cpu that holds the screen to be cleared.
      */
-    static inline void op_code_0xE0(CPU& cpu)
+    static inline void op_code_0xE0(CPU& cpu, const OpCode& op_code)
     {
         for(int i = 0 ; i < cpu.screen.size() ; i++) cpu.screen[i] = 0;
     }
@@ -62,7 +117,7 @@ namespace chip
      * 
      *  @param cpu the cpu whose PC register will be restored.
      */ 
-    static inline void op_code_0xEE(CPU& cpu)
+    static inline void op_code_0xEE(CPU& cpu, const OpCode& op_code)
     {
         cpu.PC = cpu.stack[cpu.SP - 1];
         cpu.SP -= 1;
@@ -311,26 +366,64 @@ namespace chip
         cpu.V[((op_code.data & 0xF0) >> 4)] = cpu.V[((op_code.data & 0xF0) >> 4)] << 1;
     }
 
+    /**
+     *  Skips the next instruction if the value on register Vx doens't equal
+     *  to the value on register Vy.
+     * 
+     *  @param cpu the cpu that contains the registers used by the operation.
+     *  @param op_code contains the id of the registers that will be used.
+     */ 
     static inline void op_code_0x90(CPU& cpu, const OpCode& op_code)
     {
         if(cpu.V[((op_code.data & 0xF0) >> 4)] != cpu.V[(op_code.data & 0xF)]) cpu.PC += 2;
     }
 
+    /**
+     *  Set the register I to the value NNN of the OpCode.
+     * 
+     *  @param cpu the cpu that contains the register used by the operation.
+     *  @param op_code contains the value that the register will be set.
+     */ 
     static inline void op_code_0xA(CPU& cpu, const OpCode& op_code)
     {
         cpu.I = op_code.data;
     }
 
+    /**
+     *  Jump to the NNN address plus the value on the register V0.
+     * 
+     *  @param cpu the cpu that contains the register used by the operation.
+     *  @param op_code contains the address to jump to.
+     */ 
     static inline void op_code_0xB(CPU& cpu, const OpCode& op_code)
     {
         cpu.PC = cpu.V[0] + op_code.data;
     }
 
-    static inline void op_code_0xC(CPU& cpu, const OpCode& op_code, uint8_t random = rand() % 256)
+    /**
+     *  Set the register Vx to the result of the bitwise operation between
+     *  a random number and NN.
+     * 
+     *  @param cpu the cpu that contains the register used to store the result.
+     *  @param op_code contains the id of the register and the value to be used
+     *                 by the operation.
+     */ 
+    static inline void op_code_0xC(CPU& cpu, const OpCode& op_code)
     {
+        uint8_t random = rand() % 256;
+
         cpu.V[(op_code.data & 0xF00) >> 8] = (op_code.data & 0xFF) & random; 
     }
 
+    /**
+     *  Draws a sprite at coordinate (VX, VY) that has a width of 8 
+     *  pixels and a height of N+1 pixels. Each row of 8 pixels is 
+     *  read as bit-coded starting from memory location I; I value 
+     *  doesn’t change after the execution of this instruction
+     * 
+     *  @param cpu the cpu that contains the registers used by the operation.
+     *  @param op_code contains the id of the registers that will be used.
+     */ 
     static inline void op_code_0xD(CPU& cpu, const OpCode& op_code)
     {
         const uint8_t vx = cpu.V[(op_code.data & 0xF00) >> 8];
@@ -354,61 +447,171 @@ namespace chip
         }
     }
 
+     /**
+     *  Skips the next instruction if the key stored in VX is pressed.
+     * 
+     *  @param cpu the cpu that contains the registers used by the operation.
+     *  @param op_code contains the id of the registers that will be used.
+     */ 
     static inline void op_code_0xE9E(CPU& cpu, const OpCode& op_code)
     {
-
+        if(cpu.key_pad[cpu.V[op_code.data]] == 0x1) cpu.PC += 2;
     }
 
+    /**
+     *  Skips the next instruction if the key stored in VX isn't pressed.
+     * 
+     *  @param cpu the cpu that contains the registers used by the operation.
+     *  @param op_code contains the id of the registers that will be used.
+     */ 
     static inline void op_code_0xEA1(CPU& cpu, const OpCode& op_code)
     {
-
+        if(cpu.key_pad[cpu.V[op_code.data]] != 0x1) cpu.PC += 2;
     }
 
+    /**
+     *  Skips the next instruction if the key stored in VX isn't pressed.
+     * 
+     *  @param cpu the cpu that contains the registers used by the operation.
+     *  @param op_code contains the id of the registers that will be used.
+     */ 
     static inline void op_code_0xF07(CPU& cpu, const OpCode& op_code)
     {
         cpu.V[op_code.data] = cpu.DT;
     }
 
+    /**
+     *  A key press is awaited, and then stored in VX. (Blocking Operation. 
+     *  All instruction halted until next key event)
+     * 
+     *  @param cpu the cpu that contains the registers used by the operation.
+     *  @param op_code contains the id of the registers that will be used.
+     */ 
     static inline void op_code_0xF0A(CPU& cpu, const OpCode& op_code)
     {
+        for(int i = 0 ; i < 16 ; i++) 
+        {
+            if(cpu.key_pad[i] != 0)
+            {
+                cpu.V[op_code.data] = i;
+                return;
+            }
+        }
 
+        cpu.PC -= 2;
     }
 
+    /**
+     *  Sets the delay timer to VX.
+     * 
+     *  @param cpu the cpu that contains the registers used by the operation.
+     *  @param op_code contains the id of the register that will be used.
+     */ 
     static inline void op_code_0xF15(CPU& cpu, const OpCode& op_code)
     {
         cpu.DT = cpu.V[op_code.data];
     }
 
+    /**
+     *  Sets the sound timer to VX.
+     * 
+     *  @param cpu the cpu that contains the registers used by the operation.
+     *  @param op_code contains the id of the register that will be used.
+     */ 
     static inline void op_code_0xF18(CPU& cpu, const OpCode& op_code)
     {
         cpu.ST = cpu.V[op_code.data];
     }
 
+    /**
+     *  Adds VX to I. VF is not affected
+     * 
+     *  @param cpu the cpu that contains the registers used by the operation.
+     *  @param op_code contains the id of the register that will be used.
+     */ 
     static inline void op_code_0xF1E(CPU& cpu, const OpCode& op_code)
     {
         cpu.I += cpu.V[op_code.data];
     }
 
+    /**
+     *  Sets I to the location of the sprite for the character in VX. 
+     *  Characters 0-F (in hexadecimal) are represented by a 4x5 font.
+     * 
+     *  @param cpu the cpu that contains the registers used by the operation.
+     *  @param op_code contains the id of the register that will be used.
+     */ 
     static inline void op_code_0xF29(CPU& cpu, const OpCode& op_code)
     {
-
+        cpu.I = op_code.data * 5;
     }
 
+    /**
+     *  Stores the binary-coded decimal representation of VX, 
+     *  with the most significant of three digits at the address 
+     *  in I, the middle digit at I plus 1, and the least significant 
+     *  digit at I plus 2. (In other words, take the decimal representation 
+     *  of VX, place the hundreds digit in memory at location in I, the 
+     *  tens digit at location I+1, and the ones digit at location I+2.)
+     * 
+     *  @param cpu the cpu that contains the registers used by the operation.
+     *  @param op_code contains the id of the register that will be used.
+     */ 
     static inline void op_code_0xF33(CPU& cpu, const OpCode& op_code)
     {
+        uint8_t data = cpu.V[op_code.data];
+        
+        cpu.memory[cpu.I + 2] = data % 10; 
 
+        data /= 10;
+        cpu.memory[cpu.I + 1] = data % 10;
+
+        data /= 10;
+        cpu.memory[cpu.I] = data % 10;
     }
 
+    /**
+     *  Stores V0 to VX (including VX) in memory starting at address.
+     * 
+     *  @param cpu the cpu that contains the registers used by the operation.
+     *  @param op_code contains the id of the register that will be used.
+     */ 
     static inline void op_code_0xF55(CPU& cpu, const OpCode& op_code)
     {
-
+        for(int i = 0; i <= op_code.data; i++) cpu.memory[cpu.I + i] = cpu.V[i];
     }
 
+    /**
+     *  Fills V0 to VX (including VX) with values from memory 
+     *  starting at address I. The offset from I is increased 
+     *  by 1 for each value written, but I itself is left 
+     *  unmodified.
+     * 
+     *  @param cpu the cpu that contains the registers used by the operation.
+     *  @param op_code contains the id of the register that will be used.
+     */ 
     static inline void op_code_0xF65(CPU& cpu, const OpCode& op_code)
     {
-
+        for(int i = 0; i <= op_code.data; i++) cpu.V[i] = cpu.memory[cpu.I + i];
     }
 
+    static inline void load_ROM(CPU& cpu, const std::string& rom_path)
+    {
+        std::ifstream file{rom_path.c_str(), std::ios::in | std::ios::binary | std::ios::ate};
+
+        if(!file.is_open()) throw std::runtime_error{"Unable to open ROM " + rom_path};
+        std::streampos rom_size = file.tellg();
+
+        char *file_data = new char[rom_size];
+
+        file.seekg(0, std::ios::beg);
+        file.read(&file_data[0], rom_size);
+        file.close();
+
+        for(int i = 0 ; i < rom_size ; i++) cpu.memory[ROM_START + i] = static_cast<uint8_t>(file_data[i]);
+
+        delete[] file_data;
+    }
 
     /**
      *  Given a Chip-8 binary program and a pointer to the next OpCode, determine
@@ -430,43 +633,6 @@ namespace chip
      *  NN  = two nibbles or one byte (8 bits).
      *  NNN = three niblles (12 bits). 
      * 
-     *  The possible OpCodes:
-     *  
-     *  0x00E0 -> 0xE0 = Clears the screen.
-     *  0x00EE -> 0xEE = Return from subroutine.
-     *  0x1NNN -> 0x1  = Jumps to address NNN.
-     *  0x2NNN -> 0x2  = Calls subroutine at NNN.
-     *  0x3XNN -> 0x3  = Skips the next instruction if VX equals NN.
-     *  0x4XNN -> 0x4  = Skips the next instruction if VX doesn't equal NN
-     *  0x5XY0 -> 0x50 = Skips the next instruction if VX equals VY.
-     *  0x6XNN -> 0x6  = Sets VX to NN.
-     *  0x7XNN -> 0x7  = Adds NN to VX. (Carry flag is not changed)
-     *  0x8XY0 -> 0x80 = Sets VX to the value of VY.
-     *  0x8XY1 -> 0x81 = Sets VX to VX or VY. (Bitwise OR operation)
-     *  0x8XY2 -> 0x82 = Sets VX to VX and VY. (Bitwise AND operation)
-     *  0x8XY3 -> 0x83 = Sets VX to VX xor VY.
-     *  0x8XY4 -> 0x84 = Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't.
-     *  0x8XY5 -> 0x85 = VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
-     *  0x8XY6 -> 0x86 = Stores the least significant bit of VX in VF and then shifts VX to the right by 1.
-     *  0x8XY7 -> 0x87 = Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
-     *  0x8XYE -> 0x8E = Stores the most significant bit of VX in VF and then shifts VX to the left by 1.[b].
-     *  0x9XY0 -> 0x90 = Skips the next instruction if VX doesn't equal VY. 
-     *  0xANNN -> 0xA  = Sets I to the address NNN.
-     *  0xBNNN -> 0xB  = Jumps to the address NNN plus V0.
-     *  0xCXNN -> 0xC  = Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN.
-     *  0xDXYN -> 0xD  = Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N+1 pixels. Each row of 8 pixels is read as bit-coded starting from memory location I; I value doesn’t change after the execution of this instruction. As described above, VF is set to 1 if any screen pixels are flipped from set to unset when the sprite is drawn, and to 0 if that doesn’t happen
-     *  0xEX9E -> 0xE9E = Skips the next instruction if the key stored in VX is pressed
-     *  0xEXA1 -> 0xEA1 = Skips the next instruction if the key stored in VX isn't pressed
-     *  0xFX07 -> 0xF07 = Sets VX to the value of the delay timer.
-     *  0xFX0A -> 0xF0A = A key press is awaited, and then stored in VX
-     *  0xFX15 -> 0xF15 = Sets the delay timer to VX.
-     *  0xFX18 -> 0xF18 = Sets the sound timer to VX.
-     *  0xFX1E -> 0xF1E = Adds VX to I. VF is not affected.
-     *  0xFX29 -> 0xF29 = Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) are represented by a 4x5 font.
-     *  0xFX33 -> 0xF33 = Stores the binary-coded decimal representation of VX, with the most significant of three digits at the address in I, the middle digit at I plus 1, and the least significant digit at I plus 2. (In other words, take the decimal representation of VX, place the hundreds digit in memory at location in I, the tens digit at location I+1, and the ones digit at location I+2.
-     *  0xFX55 -> 0xF55 = Stores V0 to VX (including VX) in memory starting at address I. The offset from I is increased by 1 for each value written, but I itself is left unmodified.
-     *  0xFX65 -> 0xF65 = Fills V0 to VX (including VX) with values from memory starting at address I. The offset from I is increased by 1 for each value written, but I itself is left unmodified.[d]
-     *
      *  @tparam N the number of bytes in the buffer program.
      *  
      *  @param program the buffer that contains the binary data.
@@ -507,6 +673,62 @@ namespace chip
         }
 
         return OpCode{ code , data };  
+    }
+
+    const std::unordered_map<uint16_t, std::function<void (CPU& cpu, const OpCode& op_code)>> op_codes 
+    {
+        { 0xE0,  op_code_0xE0 },
+        { 0xEE,  op_code_0xEE },
+        { 0x0 ,  op_code_0x0  }, 
+        { 0x1,   op_code_0x1  }, 
+        { 0x2,   op_code_0x2  }, 
+        { 0x3,   op_code_0x3  }, 
+        { 0x4,   op_code_0x4  }, 
+        { 0x50,  op_code_0x50 }, 
+        { 0x6,   op_code_0x6  }, 
+        { 0x7,   op_code_0x7  }, 
+        { 0x80,  op_code_0x80 }, 
+        { 0x81,  op_code_0x81 }, 
+        { 0x82,  op_code_0x82 }, 
+        { 0x83,  op_code_0x83 }, 
+        { 0x84,  op_code_0x84 }, 
+        { 0x85,  op_code_0x85 }, 
+        { 0x86,  op_code_0x86 }, 
+        { 0x87,  op_code_0x87 }, 
+        { 0x8E,  op_code_0x8E }, 
+        { 0x90,  op_code_0x90 }, 
+        { 0xA,   op_code_0xA }, 
+        { 0xB,   op_code_0xB }, 
+        { 0xC,   op_code_0xC }, 
+        { 0xD,   op_code_0xD }, 
+        { 0xE9E, op_code_0xE9E }, 
+        { 0xEA1, op_code_0xEA1 }, 
+        { 0xF07, op_code_0xF07 }, 
+        { 0xF0A, op_code_0xF0A }, 
+        { 0xF15, op_code_0xF15 }, 
+        { 0xF18, op_code_0xF18 }, 
+        { 0xF1E, op_code_0xF1E }, 
+        { 0xF29, op_code_0xF29 }, 
+        { 0xF33, op_code_0xF33 }, 
+        { 0xF55, op_code_0xF55 }, 
+        { 0xF65, op_code_0xF65 }
+    };
+
+    /**
+     *  Fetch, decode and execute an instruction
+     *  from memory. Also, increment the PC register
+     *  by two (advance to the next instruction).
+     * 
+     *  @param cpu it contains all the resources
+     *             used by the program.
+     */
+    static inline void cycle(CPU& cpu)
+    {
+        OpCode op_code = decode(cpu.memory, cpu.PC);
+
+        op_codes.at(op_code.code)(cpu, op_code);
+
+        cpu.PC += 2;
     }
 }
 
